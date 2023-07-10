145e2a726ff602c9ca22e5eb777353c7325985a2
Suraj Adhikari <sssadhikari222@gmail.com>
2023-07-10 08:49:46 +0545

    Table

--- src/synchronization/Table.java
+++ src/synchronization/Table.java
@@ -16,7 +16,7 @@ public class Table {
     void printTable(int n){
         synchronized (this){// This is a synchronized block
             for(int i =1; i<=5; i++ ){
-                System.out.println(n*i);
+                System.out.println(n+"*"+i+"="+n*i);
                 try{
                     Thread.sleep(500);
                 }catch (InterruptedException ie){
