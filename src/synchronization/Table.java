7aa559a5cdb24a6961cbb0224241b2db713cc28d
Suraj Adhikari <sssadhikari222@gmail.com>
2023-07-10 08:44:52 +0545

    Update

--- src/synchronization/Table.java
+++ src/synchronization/Table.java
@@ -1,7 +1,7 @@
 package synchronization;
 
 public class Table {
-    synchronized void printTable(int n)
+    /*synchronized void printTable(int n)
     {
         for(int i =1; i<=5; i++){
             System.out.println(n*i);
@@ -9,6 +9,19 @@ public class Table {
                 Thread.sleep(500);
             }catch(InterruptedException ie){
                 System.out.println(ie);
+            }
+        }
+    }*/
+
+    void printTable(int n){
+        synchronized (this){// This is a synchronized block
+            for(int i =1; i<=5; i++ ){
+                System.out.println(n*i);
+                try{
+                    Thread.sleep(500);
+                }catch (InterruptedException ie){
+                    System.out.println(ie);
+                }
             }
         }
     }
